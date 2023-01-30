b9b0bf6164c72c6234e06f60202a3b67174625d8
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-30 19:25:18 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -52,4 +52,8 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2)
 console.log(myConcat([1, 2], [3, 4, 5]));
 ______________________________________
 
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+______________________________________
 
