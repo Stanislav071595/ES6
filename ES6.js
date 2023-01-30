f986706bce137a69048a1c8f5f5499a979b06c7f
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-30 19:26:58 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,4 +1,16 @@
-_________________________________
+/* ES6 */
+______________________________________
+
+function checkScope() {
+  let i = 'function scope';
+  if (false) {
+    i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+______________________________________
 
 const s = [5, 7, 2];
 function editInPlace() {
