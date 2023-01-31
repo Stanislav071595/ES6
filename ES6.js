229ee4193596f6249666abd18e10dcde4deb54d8
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-31 20:01:15 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -71,3 +71,16 @@ arr2 = [...arr1];  // Change this line
 console.log(arr2);
 ______________________________________
 
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
+______________________________________
+
