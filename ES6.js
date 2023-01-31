133c7bbfb6ec98deb3e4c1ee0b3d645c2454ce31
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-01-31 19:58:01 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -63,3 +63,11 @@ const sum = (...args) => {
 }
 ______________________________________
 
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+______________________________________
+
