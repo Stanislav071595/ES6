2a415c626d5f65b60dd40a503742649430652307
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-01 18:25:32 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -128,3 +128,19 @@ const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sourceWithoutFirstTwo = removeFirstTwo(source);
 ______________________________________
 
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => {
+  return (max + min) / 2.0;
+}; 
+// Only change code above this line
+______________________________________
+
