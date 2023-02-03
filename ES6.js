9e8ec531f87e3b3b8baa619a02c83ef33af7dad0
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-03 18:42:26 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -1,4 +1,29 @@
+/* ES6 */
+______________________________________
 
+function checkScope() {
+  let i = 'function scope';
+  if (false) {
+    i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+______________________________________
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+______________________________________
 
 function freezeObj() {
   const MATH_CONSTANTS = {
