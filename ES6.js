22bab800370929ac46d64c4966b271fdade57e9c
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-05 13:56:25 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -242,3 +242,23 @@ makeServerRequest.then(result => {
 });
 ______________________________________
 
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error  => {
+  console.log(error);
+});
+______________________________________
+
