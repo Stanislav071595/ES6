6b466d4f8ea948d55b9bb084c78fea5e861bbd07
Станислав Похилько <78536011+Stanislav071595@users.noreply.github.com>
2023-02-05 13:20:59 +0300

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -203,6 +203,12 @@ export default function subtract(x, y) {
 }
 ______________________________________
 
+import subtract from './math_functions.js';  
+// Only change code above this line
+
+subtract(7,4);
+______________________________________
+
 
 
 
